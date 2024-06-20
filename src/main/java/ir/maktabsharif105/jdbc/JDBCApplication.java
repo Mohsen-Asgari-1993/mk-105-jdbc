b37ef6f6f553321d9353c1af772d150284466757
maxixking66 <maxixking66@gamil.com>
2024-06-20 16:59:51 +0330

    add lock example

--- src/main/java/ir/maktabsharif105/jdbc/JDBCApplication.java
+++ src/main/java/ir/maktabsharif105/jdbc/JDBCApplication.java
@@ -1,113 +1,56 @@
 package ir.maktabsharif105.jdbc;
 
-import ir.maktabsharif105.jdbc.domain.BaseEntity;
-import lombok.*;
+import lombok.SneakyThrows;
 
 import java.util.Random;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class JDBCApplication {
 
     static Random random = new Random();
 
+    static ReentrantLock lock = new ReentrantLock();
+
+
+    @SneakyThrows
     public static void main(String[] args) {
-        ExecutorService executorService = Executors.newFixedThreadPool(10);
-        CompletableFuture.allOf(
-                CompletableFuture.runAsync(
-                        () -> printWithSleep(), executorService
-                ),
-                CompletableFuture.runAsync(
-                        () -> printWithSleep(), executorService
-                ),
-                CompletableFuture.runAsync(
-                        () -> printWithSleep(), executorService
-                ),
-                CompletableFuture.runAsync(
-                        () -> printWithSleep(), executorService
-                ),
-                CompletableFuture.runAsync(
-                        () -> printWithSleep(), executorService
-                ),
-                CompletableFuture.runAsync(
-                        () -> printWithSleep(), executorService
-                ),
-                CompletableFuture.runAsync(
-                        () -> printWithSleep(), executorService
-                ),
-                CompletableFuture.runAsync(
-                        () -> printWithSleep(), executorService
-                )
-        ).join();
-        executorService.shutdown();
+
+        Thread thread = new Thread(
+                () -> {
+                    tryToLock();
+                }
+        );
+        lock.lock();
+        System.out.println(lock.getHoldCount());
+        lock.lock();
+        System.out.println(lock.getHoldCount());
+        System.out.println("after lock thread " + Thread.currentThread().getName());
+        thread.start();
+        System.out.println("after start");
+        Thread.sleep(1500);
+        System.out.println("after sleep");
+        lock.unlock();
+        System.out.println(lock.getHoldCount());
+        lock.unlock();
+        System.out.println(lock.getHoldCount());
+        System.out.println("after unlock thread " + Thread.currentThread().getName());
     }
 
-    private static void printWithSleep() {
-        System.out.println("start Thread: " + Thread.currentThread().getName());
+    private static void tryToLock() {
+        System.out.println("try to lock thread: " + Thread.currentThread().getName());
+        System.out.println("thread: " + Thread.currentThread().getName() + " - " + lock.getHoldCount());
+        lock.lock();
+        System.out.println("thread: " + Thread.currentThread().getName() + " - " + lock.getHoldCount());
+        System.out.println("start thread: " + Thread.currentThread().getName());
         try {
-            Thread.sleep(random.nextLong(500, 2500));
+            Thread.sleep(1000);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-        System.out.println("end Thread: " + Thread.currentThread().getName());
+        System.out.println("end thread: " + Thread.currentThread().getName());
+        System.out.println("thread: " + Thread.currentThread().getName() + " - " + lock.getHoldCount());
+        lock.unlock();
+        System.out.println("thread: " + Thread.currentThread().getName() + " - " + lock.getHoldCount());
     }
-
-    private static void createLogFirstVersion(BaseEntity city) {
-        new Thread(
-                () -> {
-                    try {
-                        Thread.sleep(500);
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
-                    System.out.println("log created!!!");
-                }
-        ).start();
-    }
-
-    @SneakyThrows
-    private static void setTotalCounts(DTO dto) {
-        long sleep = random.nextLong(1000, 3000);
-        System.out.println("setTotalCounts sleep: " + sleep);
-        Thread.sleep(sleep);
-        dto.setTotalCounts(random.nextLong(1, 100));
-
-    }
-
-    @SneakyThrows
-    private static void setClosedCounts(DTO dto) {
-        long sleep = random.nextLong(2000, 5000);
-        System.out.println("setClosedCounts sleep: " + sleep);
-        Thread.sleep(sleep);
-        dto.setTotalCounts(random.nextLong(1, 100));
-    }
-
-    @SneakyThrows
-    private static void setRejectedCounts(DTO dto) {
-        long sleep = random.nextLong(200, 1000);
-        System.out.println("setRejectedCounts sleep: " + sleep);
-        Thread.sleep(sleep);
-        dto.setRejectedCounts(random.nextLong(1, 100));
-    }
-}
-
-@Setter
-@Getter
-@NoArgsConstructor
-@ToString
-class DTO {
-
-    private long totalCounts;
-
-    private long closedCounts;
-
-    private long rejectedCounts;
-
-    private long waitingCounts;
-
-    private long acceptedCounts;
-
-//    TODO override getter
 
 }
