17f1c4233a2acb212178ecfd6db968d9888267ad
maxixking66 <maxixking66@gamil.com>
2024-06-20 10:35:26 +0330

    create new thread with runnable 3

--- src/main/java/ir/maktabsharif105/jdbc/JDBCApplication.java
+++ src/main/java/ir/maktabsharif105/jdbc/JDBCApplication.java
@@ -10,42 +10,22 @@ public class JDBCApplication {
     public static void main(String[] args) {
         System.out.println("start: " + Thread.currentThread());
 
-        Runnable runnable = () -> {
-            System.out.println("start: " + Thread.currentThread());
-            for (int i = 0; i < 4; i++) {
-                System.out.println(LocalTime.now());
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-            System.out.println("end: " + Thread.currentThread());
-        };
-
         Thread thread = new Thread(
-                runnable
+                () -> {
+                    System.out.println("start: " + Thread.currentThread());
+                    for (int i = 0; i < 4; i++) {
+                        System.out.println(LocalTime.now());
+                        try {
+                            Thread.sleep(1000);
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                    System.out.println("end: " + Thread.currentThread());
+                }
         );
         thread.start();
 
-        System.out.println("end: " + Thread.currentThread());
-    }
-}
-
-class MySimpleRunnable implements Runnable {
-
-//    public MySimpleThread() {
-//        start();
-//    }
-
-    @Override
-    @SneakyThrows
-    public void run() {
-        System.out.println("start: " + Thread.currentThread());
-        for (int i = 0; i < 4; i++) {
-            System.out.println(LocalTime.now());
-            Thread.sleep(1000);
-        }
         System.out.println("end: " + Thread.currentThread());
     }
 }
