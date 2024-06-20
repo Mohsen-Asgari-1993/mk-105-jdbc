b215d267e8004e49517c367ab4618b04289c738b
maxixking66 <maxixking66@gamil.com>
2024-06-20 16:37:18 +0330

    add Future example 2

--- src/main/java/ir/maktabsharif105/jdbc/JDBCApplication.java
+++ src/main/java/ir/maktabsharif105/jdbc/JDBCApplication.java
@@ -1,59 +1,56 @@
 package ir.maktabsharif105.jdbc;
 
 import ir.maktabsharif105.jdbc.domain.BaseEntity;
-import ir.maktabsharif105.jdbc.repository.CityRepository;
-import ir.maktabsharif105.jdbc.repository.impl.CityRepositoryImpl;
-import ir.maktabsharif105.jdbc.util.ApplicationProperties;
 import lombok.*;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.time.Duration;
-import java.time.LocalTime;
 import java.util.Random;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class JDBCApplication {
 
     static Random random = new Random();
 
-    @SneakyThrows
     public static void main(String[] args) {
-
-        Connection connection = DriverManager.getConnection(
-                ApplicationProperties.DB_URL,
-                ApplicationProperties.DB_USERNAME,
-                ApplicationProperties.DB_PASSWORD
-        );
-
-        LocalTime start = LocalTime.now();
-        CityRepository cityRepository = new CityRepositoryImpl(connection);
-        BaseEntity city = cityRepository.findById(10L);
-        createLog(city);
-        LocalTime end = LocalTime.now();
-        System.out.println(city + " - Duration: " + Duration.between(start, end).toMillis());
-        Thread.sleep(1000);
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        CompletableFuture.allOf(
+                CompletableFuture.runAsync(
+                        () -> printWithSleep(), executorService
+                ),
+                CompletableFuture.runAsync(
+                        () -> printWithSleep(), executorService
+                ),
+                CompletableFuture.runAsync(
+                        () -> printWithSleep(), executorService
+                ),
+                CompletableFuture.runAsync(
+                        () -> printWithSleep(), executorService
+                ),
+                CompletableFuture.runAsync(
+                        () -> printWithSleep(), executorService
+                ),
+                CompletableFuture.runAsync(
+                        () -> printWithSleep(), executorService
+                ),
+                CompletableFuture.runAsync(
+                        () -> printWithSleep(), executorService
+                ),
+                CompletableFuture.runAsync(
+                        () -> printWithSleep(), executorService
+                )
+        ).join();
+        executorService.shutdown();
     }
 
-    @SneakyThrows
-    private static void createLog(BaseEntity city) {
-//        createLogFirstVersion(city);
-
-//        ExecutorService executorService =
-//                Executors.newFixedThreadPool(2);
-
-        CompletableFuture.runAsync(
-                () -> {
-                    try {
-                        Thread.sleep(500);
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
-                    System.out.println("log created!!!");
-                }
-
-        );
-
+    private static void printWithSleep() {
+        System.out.println("start Thread: " + Thread.currentThread().getName());
+        try {
+            Thread.sleep(random.nextLong(500, 2500));
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("end Thread: " + Thread.currentThread().getName());
     }
 
     private static void createLogFirstVersion(BaseEntity city) {
