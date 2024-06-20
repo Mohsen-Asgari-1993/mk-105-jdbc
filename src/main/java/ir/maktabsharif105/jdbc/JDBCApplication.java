506e4ce62b7407abf9a7afca292acc0be2fc795a
maxixking66 <maxixking66@gamil.com>
2024-06-20 10:53:26 +0330

    add example for interrupt

--- src/main/java/ir/maktabsharif105/jdbc/JDBCApplication.java
+++ src/main/java/ir/maktabsharif105/jdbc/JDBCApplication.java
@@ -9,23 +9,26 @@ public class JDBCApplication {
     @SneakyThrows
     public static void main(String[] args) {
         System.out.println("start: " + Thread.currentThread());
-
-        Thread thread = new Thread(
-                () -> {
-                    System.out.println("start: " + Thread.currentThread());
-                    for (int i = 0; i < 4; i++) {
-                        System.out.println(LocalTime.now());
-                        try {
-                            Thread.sleep(1000);
-                        } catch (InterruptedException e) {
-                            throw new RuntimeException(e);
-                        }
-                    }
-                    System.out.println("end: " + Thread.currentThread());
-                }
-        );
-        thread.start();
-
+        Timer timer = new Timer();
+        timer.start();
+        Thread.sleep(20);
+        timer.interrupt();
         System.out.println("end: " + Thread.currentThread());
+    }
+}
+
+class Timer extends Thread {
+
+    @Override
+    @SneakyThrows
+    public void run() {
+        while (true) {
+            if (Thread.interrupted()) {
+                System.out.println("Timer was interrupted");
+                return;
+            } else {
+                System.out.println(LocalTime.now());
+            }
+        }
     }
 }
