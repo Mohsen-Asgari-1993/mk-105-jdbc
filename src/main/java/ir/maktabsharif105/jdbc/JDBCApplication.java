8afb21eb208a9e5d98e5a32b616a5e14cc38123b
maxixking66 <maxixking66@gamil.com>
2024-06-20 10:35:17 +0330

    create new thread with runnable 2

--- src/main/java/ir/maktabsharif105/jdbc/JDBCApplication.java
+++ src/main/java/ir/maktabsharif105/jdbc/JDBCApplication.java
@@ -10,8 +10,21 @@ public class JDBCApplication {
     public static void main(String[] args) {
         System.out.println("start: " + Thread.currentThread());
 
+        Runnable runnable = () -> {
+            System.out.println("start: " + Thread.currentThread());
+            for (int i = 0; i < 4; i++) {
+                System.out.println(LocalTime.now());
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            System.out.println("end: " + Thread.currentThread());
+        };
+
         Thread thread = new Thread(
-                new MySimpleRunnable()
+                runnable
         );
         thread.start();
 
