30af8c4827acae6d04fff324b8dc8dfee0de963f
maxixking66 <maxixking66@gamil.com>
2024-06-27 09:59:08 +0330

    add lambda expression example 3

--- src/main/java/ir/maktabsharif105/jdbc/JDBCApplication.java
+++ src/main/java/ir/maktabsharif105/jdbc/JDBCApplication.java
@@ -2,30 +2,52 @@ package ir.maktabsharif105.jdbc;
 
 import lombok.SneakyThrows;
 
+import java.util.function.DoubleBinaryOperator;
+
 public class JDBCApplication {
 
     @SneakyThrows
     public static void main(String[] args) {
 
-        Runnable runnable = () -> System.out.println("in runnable");
-
-        Thread firstThread = new Thread(runnable);
-        Thread secondThread = new Thread(
-                () -> System.out.println("anything")
+        double x = 10;
+        double y = 5;
+        System.out.println(
+                "DIVIDE: " + Operation.DIVIDE.apply(x, y)
+        );
+        System.out.println(
+                "PLUS: " + Operation.PLUS.apply(x, y)
         );
     }
 }
 
-interface Hero {
+enum Operation {
+    PLUS("+", (x, y) -> x + y),
+    MINUS("-", (x, y) -> x - y),
+    TIMES("*", (x, y) -> x * y),
+    DIVIDE("/", (x, y) -> x / y);
 
-    void fight();
 
-    static void swim() {
+    private final String symbol;
+    private final DoubleBinaryOperator op;
 
+    Operation(String symbol, DoubleBinaryOperator op) {
+        this.symbol = symbol;
+        this.op = op;
     }
 
-    default void fly() {
-
+    @Override
+    public String toString() {
+        return symbol;
     }
 
+    public double apply(double x, double y) {
+//        return switch (this) {
+//            case PLUS -> x + y;
+//            case MINUS -> x - y;
+//            case TIMES -> x * y;
+//            case DIVIDE -> x / y;
+//        };
+        return op.applyAsDouble(x, y);
+    }
 }
+
