e01cae1f6c0a693dc1b0a7b4c82d4ae76b42fe56
maxixking66 <maxixking66@gamil.com>
2024-06-27 15:42:46 +0330

    add stream example: Collectors.groupingBy

--- src/main/java/ir/maktabsharif105/jdbc/JDBCApplication.java
+++ src/main/java/ir/maktabsharif105/jdbc/JDBCApplication.java
@@ -1,34 +1,35 @@
 package ir.maktabsharif105.jdbc;
 
+import com.github.javafaker.Faker;
 import lombok.SneakyThrows;
 
-import java.util.Collection;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Stream;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class JDBCApplication {
 
     @SneakyThrows
     public static void main(String[] args) {
 
-        List<List<String>> lists = List.of(
-                List.of("x"),
-                List.of("x", "x"),
-                List.of("x", "x", "x"),
-                List.of("a")
-        );
+        Faker faker = new Faker();
 
-        System.out.println(lists);
+        List<String> words = new ArrayList<>();
 
-        Stream<String> stringStream = lists.stream().flatMap(Collection::stream);
-
-        stringStream.findFirst().ifPresent(data -> System.out.println(
-                String.join(
-                        "$", data.toUpperCase(), "1", "2"
-                )
+        IntStream.range(0, 30).forEach(i -> words.add(
+                faker.name().firstName()
         ));
 
+        Map<Integer, List<String>> collect = words.stream()
+                .collect(Collectors.groupingBy(String::length));
 
+        System.out.println(collect);
+
+        collect.entrySet().forEach(System.out::println);
+
+//         1 = {10 = [Christiane], 2 = { 8 = [Bernardo, Valentin]}, 4 = { 3= [Joi, Rex, Gil, Lyn], 4=[Mike, Gigi, Jack, Maud]}}
     }
 
 }
