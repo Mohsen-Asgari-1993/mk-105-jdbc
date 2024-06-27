1b8c7f9ddc1677d3d925a23702a347336dec0755
maxixking66 <maxixking66@gamil.com>
2024-06-27 14:45:11 +0330

    add stream example 2 flatMap method

--- src/main/java/ir/maktabsharif105/jdbc/JDBCApplication.java
+++ src/main/java/ir/maktabsharif105/jdbc/JDBCApplication.java
@@ -1,9 +1,7 @@
 package ir.maktabsharif105.jdbc;
 
-import ir.maktabsharif105.jdbc.domain.City;
 import lombok.SneakyThrows;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.stream.Stream;
@@ -13,20 +11,20 @@ public class JDBCApplication {
     @SneakyThrows
     public static void main(String[] args) {
 
-        List<Integer> integers = List.of(
-                1, 15, 3, 45, 5, 16, 7, 81, 9, 10
+        List<List<String>> lists = List.of(
+                List.of("a"),
+                List.of("b", "c"),
+                List.of("d", "e", "f"),
+                List.of("g")
         );
 
-        Stream<Integer> integerStream = integers.stream();
-        List<Integer> sortedList = integerStream.sorted(
-                Integer::compareTo
-        ).toList();
-        System.out.println(sortedList);
+        System.out.println(lists);
 
-    }
+        Stream<String> stringStream = lists.stream().flatMap(Collection::stream);
+        System.out.println(
+                stringStream.toList()
+        );
 
-    public static List<City> findAllByIdIsIn(Collection<Integer> ids) {
-        return new ArrayList<>();
     }
 
 }
