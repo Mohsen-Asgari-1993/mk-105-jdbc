1aa72ac2875ea1dda356dd139ffd13e0a2668507
maxixking66 <maxixking66@gamil.com>
2024-06-27 09:25:40 +0330

    add example for anonymous class

--- src/main/java/ir/maktabsharif105/jdbc/JDBCApplication.java
+++ src/main/java/ir/maktabsharif105/jdbc/JDBCApplication.java
@@ -1,86 +1,54 @@
 package ir.maktabsharif105.jdbc;
 
-import ir.maktabsharif105.jdbc.util.SemaphoreUtil;
 import lombok.SneakyThrows;
-
-import java.util.Random;
-import java.util.concurrent.Semaphore;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.locks.ReentrantLock;
 
 public class JDBCApplication {
 
-    static Random random = new Random();
-
-    static ReentrantLock lock = new ReentrantLock();
-
-    static Semaphore semaphore = new Semaphore(1);
-
     @SneakyThrows
     public static void main(String[] args) {
+        Hero hero = new Hero() {
+            @Override
+            public void fight() {
+                System.out.println("fly");
+            }
 
-        SemaphoreUtil.acquireCreateAdvert();
-        try {
-            System.out.println("logic");
-        } finally {
-            SemaphoreUtil.releaseCreateAdvert();
-        }
+            @Override
+            public void swim() {
 
-        Long customerId = 5L;
-        SemaphoreUtil.acquireCustomerSemaphore(5L);
-        try {
-            System.out.println("logic");
-        } finally {
-            SemaphoreUtil.releaseCustomerSemaphore(5L);
-        }
+            }
 
-        Thread thread = new Thread(
-                () -> {
-                    try {
-                        semaphore.acquire();
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
-                    System.out.println("start thread: " + Thread.currentThread().getName());
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
-                    System.out.println("end thread: " + Thread.currentThread().getName());
-                    semaphore.release();
-                }
-        );
-        semaphore.acquire();
-        if (semaphore.tryAcquire(3, TimeUnit.SECONDS)) {
+            @Override
+            public void fly() {
 
-        } else {
-            throw new RuntimeException();
-        }
+            }
+        };
 
-        thread.start();
-        System.out.println("after start");
-        Thread.sleep(1500);
-        System.out.println("after sleep");
-        semaphore.release();
-        System.out.println("after release thread " + Thread.currentThread().getName());
+        Hero second = new Hero() {
+            @Override
+            public void fight() {
+
+            }
+
+            @Override
+            public void swim() {
+
+            }
+
+            @Override
+            public void fly() {
+
+            }
+        };
+
+
     }
+}
 
-    private static void tryToLock() {
-        System.out.println("try to lock thread: " + Thread.currentThread().getName());
-        System.out.println("thread: " + Thread.currentThread().getName() + " - " + lock.getHoldCount());
-        lock.lock();
-        System.out.println("thread: " + Thread.currentThread().getName() + " - " + lock.getHoldCount());
-        System.out.println("start thread: " + Thread.currentThread().getName());
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-        System.out.println("end thread: " + Thread.currentThread().getName());
-        System.out.println("thread: " + Thread.currentThread().getName() + " - " + lock.getHoldCount());
-        lock.unlock();
-        System.out.println("thread: " + Thread.currentThread().getName() + " - " + lock.getHoldCount());
-    }
+interface Hero {
 
+    void fight();
+
+    void swim();
+
+    void fly();
 }
