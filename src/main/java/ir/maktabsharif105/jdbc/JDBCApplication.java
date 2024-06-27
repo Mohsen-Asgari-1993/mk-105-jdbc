5340fe19372b25d430cd9e6675b648ea1716f6f3
maxixking66 <maxixking66@gamil.com>
2024-06-27 14:53:40 +0330

    add stream example: match

--- src/main/java/ir/maktabsharif105/jdbc/JDBCApplication.java
+++ src/main/java/ir/maktabsharif105/jdbc/JDBCApplication.java
@@ -12,17 +12,19 @@ public class JDBCApplication {
     public static void main(String[] args) {
 
         List<List<String>> lists = List.of(
-                List.of("a"),
-                List.of("b", "c"),
-                List.of("d", "e", "f"),
-                List.of("g")
+                List.of("x"),
+                List.of("x", "x"),
+                List.of("x", "x", "x"),
+                List.of("a")
         );
 
         System.out.println(lists);
 
         Stream<String> stringStream = lists.stream().flatMap(Collection::stream);
-//        System.out.println(stringStream.count());
-        stringStream.forEach(System.out::println);
+
+//        System.out.println("contains x: " + stringStream.anyMatch(data -> data.equals("x")));
+//        System.out.println("contains x: " + stringStream.noneMatch(data -> data.equals("x")));
+        System.out.println("contains x: " + stringStream.allMatch(data -> data.equals("x")));
 
     }
 
