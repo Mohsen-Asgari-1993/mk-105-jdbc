da0c149e224e5c5b24fb9b56f1f61a6be94cbfe9
maxixking66 <maxixking66@gamil.com>
2024-06-27 10:28:15 +0330

    add method reference example 2

--- src/main/java/ir/maktabsharif105/jdbc/JDBCApplication.java
+++ src/main/java/ir/maktabsharif105/jdbc/JDBCApplication.java
@@ -1,23 +1,57 @@
 package ir.maktabsharif105.jdbc;
 
-import lombok.SneakyThrows;
+import lombok.*;
 
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 
 public class JDBCApplication {
 
     @SneakyThrows
     public static void main(String[] args) {
-        List<String> words = List.of(
-                "mohsen", "ali", "mahdi", "reza", "amirali", "shayan"
+        List<String> numbers = List.of(
+                "10", "15", "13", "7", "0", "55"
         );
-        System.out.println();
-        words.forEach(JDBCApplication::print);
-        words.forEach(System.out::println);
+
+
+        numbers.forEach(t -> System.out.println(t));
+        numbers.forEach(System.out::println);
+
+
+        numbers.forEach(t -> Integer.parseInt(t));
+        numbers.forEach(Integer::parseInt);
+        numbers.forEach(MyWrapper::new);
+        numbers.forEach(String::toUpperCase);
+        numbers.forEach(MyWrapper::new);
+
+        List<Integer> arrayLengths = List.of(1, 5, 6);
+        arrayLengths.stream()
+                .map(length -> new int[length]);
+        arrayLengths.stream()
+                .map(int[]::new);
+
+        List<Instant> instants = List.of(
+                Instant.now().minus(5, ChronoUnit.DAYS),
+                Instant.now().minus(2, ChronoUnit.DAYS),
+                Instant.now().minus(2, ChronoUnit.HOURS)
+        );
+        instants.forEach(instant -> Instant.now().isAfter(instant));
+        instants.forEach(Instant.now()::isAfter);
 
     }
 
     public static void print(Object o) {
         System.out.println(o);
     }
+}
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+class MyWrapper {
+    private String myField;
+
+
 }
