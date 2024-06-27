5acacb5f82f51c4fb2e9e0c59874662fcd6b8584
maxixking66 <maxixking66@gamil.com>
2024-06-27 09:36:52 +0330

    add lambda expression example 2

--- src/main/java/ir/maktabsharif105/jdbc/JDBCApplication.java
+++ src/main/java/ir/maktabsharif105/jdbc/JDBCApplication.java
@@ -2,41 +2,17 @@ package ir.maktabsharif105.jdbc;
 
 import lombok.SneakyThrows;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class JDBCApplication {
 
     @SneakyThrows
     public static void main(String[] args) {
 
-        List<String> words = new ArrayList<>(
-                List.of(
-                        "mohsen", "ali", "mahdi", "reza", "amirali", "shayan"
-                )
+        Runnable runnable = () -> System.out.println("in runnable");
+
+        Thread firstThread = new Thread(runnable);
+        Thread secondThread = new Thread(
+                () -> System.out.println("anything")
         );
-
-        System.out.println(words);
-
-//        Collections.sort(
-//                words,
-//                new Comparator<String>() {
-//                    @Override
-//                    public int compare(String o1, String o2) {
-//                        return Integer.compare(o1.length(), o2.length());
-//                    }
-//                }
-//        );
-//
-//        words.sort((o1, o2) -> {
-//            System.out.println("in concrete method:");
-//            return Integer.compare(o1.length(), o2.length());
-//        });
-
-        words.sort((o1, o2) -> Integer.compare(o1.length(), o2.length()));
-        System.out.println(words);
-
-
     }
 }
 
