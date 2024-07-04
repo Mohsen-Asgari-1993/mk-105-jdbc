098eb057f7f953cebe9aae37cfaa9c7b8e9d2d6f
maxixking66 <maxixking66@gamil.com>
2024-07-04 16:37:21 +0330

    change module

--- main-app/src/main/java/ir/maktabsharif105/jdbc/JDBCApplication.java
+++ main-app/src/main/java/ir/maktabsharif105/jdbc/JDBCApplication.java
@@ -4,11 +4,15 @@ import ir.maktabsharif105.jdbc.domain.ProductCategory;
 import ir.maktabsharif105.jdbc.repository.ProductCategoryRepository;
 import ir.maktabsharif105.jdbc.repository.impl.ProductCategoryRepositoryImpl;
 import ir.maktabsharif105.jdbc.util.ConnectionUtil;
+import ir.maktabsharif115.smsprovider.KavenegarSender;
+import ir.maktabsharif115.smsprovider.dto.SmsRequestDTO;
+import ir.maktabsharif115.smsprovider.enumeration.SmsPurpose;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.SneakyThrows;
 
+import java.util.LinkedHashMap;
 import java.util.List;
 
 @Setter
@@ -23,5 +27,14 @@ public class JDBCApplication {
 
         List<ProductCategory> allRootWithChildren = productCategoryRepository.findAllRootWithChildren();
         allRootWithChildren.forEach(System.out::println);
+
+        KavenegarSender sender = new KavenegarSender();
+        sender.send(
+                new SmsRequestDTO(
+                        "09121111111",
+                        SmsPurpose.OTP,
+                        new LinkedHashMap<>()
+                )
+        );
     }
 }
